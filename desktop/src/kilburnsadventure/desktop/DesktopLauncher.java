e536b2ccd1e4b84d26f2502dc7c11c61d649843b
Cam Edwards <cameronedwards145@gmail.com>
2014-11-28 23:33:40 +0000

    Initial Commit!

---
+++ desktop/src/kilburnsadventure/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package kilburnsadventure.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import kilburnsadventure.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
