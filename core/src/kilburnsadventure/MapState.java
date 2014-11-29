0d1562b80af7241ee8ba3d9fdc3940598495d4aa
Mario Stefanov <stefanovn19@yahoo.co.uk>
2014-11-29 09:32:27 +0000

    MapState 30 percent

--- core/src/kilburnsadventure/MapState.java
+++ core/src/kilburnsadventure/MapState.java
@@ -2,6 +2,7 @@ package kilburnsadventure;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
@@ -12,15 +13,14 @@ public class MapState extends GameState
 {
 	private Texture map;
 	private int mapID;
-	private List<GameObject> objects = new ArrayList<GameObject>();
-	private List<GameObject> toAdd = new ArrayList<GameObject>();
-	private List<GameObject> toRemove = new ArrayList<GameObject>();
+	private List<MapObject> objects = new ArrayList<MapObject>();
+	private List<MapObject> toAdd = new ArrayList<MapObject>();
+	private List<MapObject> toRemove = new ArrayList<MapObject>();
 	private String[] mapBackgrounds;
 	private Player player;
 	private Enemy pilots;
 	private Game game;
 	private float lastSpawnTime;
-	private int spawnPosition;
 	
 	public MapState(Game requiredGame, StateManager stateManager,int requiredMapID)
 	{
@@ -32,43 +32,49 @@ public class MapState extends GameState
 		mapBackgrounds[0] = "lvl1.jpg";
 		mapBackgrounds[1] = "lvl2.jpg";
 		map = new Texture("graphics/maps/" + mapBackgrounds[mapID]);
-		player = new Player(game,0,0);
+		player = new Player(game,this, 0,0);
 		objects.add(player);
 	}
 	
-	public List<GameObject> getObjectList()
+	public List<MapObject> getObjectList()
 	{
 		return objects;
 	}
 	
-	public boolean addToObjectList(GameObject object)
+	public boolean addToObjectList(MapObject object)
 	{
 		return toAdd.add(object);
 	}
 	
-	public boolean removeFromObjectList(GameObject object)
+	public boolean removeFromObjectList(MapObject object)
 	{
 		return toRemove.add(object);
 	}
 	
-	public void spawnEnemies(int noOfEnemies, int position)
+	public void spawnEnemies(int noOfEnemies)
 	{
-		float xPlayerPos = player.getPlayerPos().x;
-		//float yPlayerPos = player.getPlayerPos().y;
-		
-		spawnPosition = position;
+		float xPlayerPos = player.getPosition().x;
 		if (xPlayerPos > 300)
 		{
 			int different = 0;
 			for(int i = 0; i < noOfEnemies; i++)
 			{
-				different += 50;
-				pilots = new Enemy(game, this, position,different);
+				different += 200;
+				pilots = new Enemy(game, this, different, randInt(50,500));
 				addToObjectList(pilots);
 			}
 		}
 		
 		lastSpawnTime = TimeUtils.nanoTime();
+	}
+	
+	//
+	public static int randInt(int min, int max) 
+	{
+    Random rand = new Random();
+    int randomNum = rand.nextInt((max - min) + 1) + min;
+
+    return randomNum;
 	}
 	
 	@Override
@@ -79,9 +85,9 @@ public class MapState extends GameState
 		{
 			objects.get(object).update();
 		}
-		gameRef.cameraLookAt(player.getPlayerPos());
+		gameRef.cameraLookAt(player.getPosition());
 		if(TimeUtils.nanoTime() - lastSpawnTime > 1000000000)
-			spawnEnemies(5, 1);
+			spawnEnemies(5);
 		
 		for(int object = 0; object < toAdd.size(); object++)
 		{
