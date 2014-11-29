eb75928f3f4d2e329555fc8418a643c2483e461c
Mario Stefanov <stefanovn19@yahoo.co.uk>
2014-11-29 08:42:51 +0000

    Enemies moving

--- core/src/kilburnsadventure/MapState.java
+++ core/src/kilburnsadventure/MapState.java
@@ -6,6 +6,7 @@ import java.util.List;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.TimeUtils;
 
 public class MapState extends GameState 
 {
@@ -15,21 +16,21 @@ public class MapState extends GameState
 	private String[] mapBackgrounds;
 	private Player player;
 	private Enemy pilots;
+	private Game game;
+	private float lastSpawnTime;
 	
-	public MapState(Game game, StateManager stateManager,int requiredMapID)
+	public MapState(Game requiredGame, StateManager stateManager,int requiredMapID)
 	{
-		super(game, stateManager);
+		super(requiredGame, stateManager);
 		// Store the given mapID.
 		mapID = requiredMapID;
-		
+		game = requiredGame;
 		mapBackgrounds = new String[10];
 		mapBackgrounds[0] = "lvl1.jpg";
 		mapBackgrounds[1] = "lvl2.jpg";
 		map = new Texture("graphics/maps/" + mapBackgrounds[mapID]);
 		player = new Player(game,0,0);
-		pilots = new Enemy(game,600,100);
 		objects.add(player);
-		objects.add(pilots);
 	}
 	
 	public List<GameObject> getObjectList()
@@ -47,6 +48,25 @@ public class MapState extends GameState
 		return objects.remove(object);
 	}
 	
+	public void spawnEnemies(int noOfEnemies, int waves)
+	{
+		float xPlayerPos = player.getPlayerPos().x;
+		//float yPlayerPos = player.getPlayerPos().y;
+		
+		if (xPlayerPos > 300)
+		{
+			for(int i = 0; i < noOfEnemies; i++)
+			{
+				int different = 0;
+				different += i + 50;
+				pilots = new Enemy(game,800,different);
+				addToObjectList(pilots);
+			}
+		}
+		
+		lastSpawnTime = TimeUtils.nanoTime();
+	}
+	
 	@Override
 	public void update()
 	{
@@ -56,6 +76,8 @@ public class MapState extends GameState
 			objects.get(object).update();
 		}
 		gameRef.cameraLookAt(player.getPlayerPos());
+		if(TimeUtils.nanoTime() - lastSpawnTime > 1000000000)
+			spawnEnemies(10, 1);
 	}
 	
 	@Override
