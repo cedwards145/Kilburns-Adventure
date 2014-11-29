6644abe276cf45321c4108bfc6e7f87069ac8c71
Bingw12138 <wbq-live@live.cn>
2014-11-29 02:23:34 +0000

    added player

---
+++ core/src/kilburnsadventure/Player.java
@@ -0,0 +1,27 @@
+package kilburnsadventure;
+
+public class Player extends GameObject{
+
+	//player constructor
+	public Player{
+		
+	}
+	
+	public update{
+		
+	}
+	
+	public draw{
+		
+	}
+	
+	public updateMotion{
+		private boolean leftMove, rightMove, upMove, downMove;
+		leftMove = Gdx.input.isKeyPressed(keys.LEFT);
+		rightMove = Gdx.input.isKeyPressed(keys.Right);
+		upMove = Gdx.input.isKeyPressed(keys.UP);
+		downMove = Gdx.input.isKeyPressed(keys.DOWN);
+		
+		
+	}
+}
