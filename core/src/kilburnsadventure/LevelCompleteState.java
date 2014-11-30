fdc08d122006c94b0aba5b3b88956ecbcdca60b0
Bingw12138 <wbq-live@live.cn>
2014-11-30 21:27:13 +0000

    level complete finish

---
+++ core/src/kilburnsadventure/LevelCompleteState.java
@@ -0,0 +1,71 @@
+package kilburnsadventure;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+
+public class LevelCompleteState extends GameState{
+
+	private Texture levelCompleteImage;
+	private Rectangle continueButton;
+	private Rectangle quitButton;
+    private MapState map;
+	
+	
+	public LevelCompleteState(Game game, StateManager manager, MapState reqMap)
+	{
+		super(game, manager);
+		levelCompleteImage = new Texture("graphics/nextgame.png");
+		continueButton = new Rectangle(268, 260, 262, 42);
+		quitButton = new Rectangle(268, 320, 262, 42);
+		map = reqMap;
+		map.enabled = false;
+	}
+	
+	@Override
+	public void draw(SpriteBatch spriteBatch)
+	{
+		Vector2 cameraOffset = new Vector2(gameRef.getCameraPosition().x - gameRef.getWidth() / 2, 
+                gameRef.getCameraPosition().y - gameRef.getHeight() / 2);
+		spriteBatch.draw(levelCompleteImage, cameraOffset.x, cameraOffset.y);
+	}
+	
+	@Override
+	public void update()
+	{
+		if(Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE))
+		{
+			stateManager.removeState(this);
+			map.enabled = true;
+		}
+		
+		if (Gdx.input.isTouched())
+		{
+				
+			Vector3 touchPoint = gameRef.getCamera().unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
+			
+			touchPoint.x -= (gameRef.getCameraPosition().x - gameRef.getWidth() / 2);
+			touchPoint.y -= (gameRef.getCameraPosition().y - gameRef.getHeight() / 2);
+			
+			//continue to next level
+			if(continueButton.contains(touchPoint.x, touchPoint.y))
+			{
+				stateManager.removeState(this);
+				stateManager.removeState(map);
+				stateManager.addState(new MapState(gameRef,  stateManager, map.getMapLvl() + 1));
+			}
+			//quit
+			else if (quitButton.contains(touchPoint.x, touchPoint.y))
+			{
+				gameRef.resetCamera();
+				stateManager.removeState(this);
+				stateManager.removeState(map);
+				stateManager.addState(new TitleState(gameRef,  stateManager));
+			}
+		}
+	}
+}
